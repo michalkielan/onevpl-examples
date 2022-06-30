ded080e22f552de638f724a2c23745d424fb1969
Michal Kielan <michalkielan@protonmail.com>
2022-06-29 19:37:07 -0700

    Move string->fourcc enum mapping to separate file

---
+++ src/mapping.hpp
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+#include <map>
+
+#include "vpl/preview/vpl.hpp"
+
+extern const std::map<std::string, oneapi::vpl::codec_format_fourcc> codec_formats;
+
+extern const std::map<std::string, oneapi::vpl::chroma_format_idc> chroma_formats;
+
+extern const std::map<std::string, oneapi::vpl::color_format_fourcc> color_formats;
